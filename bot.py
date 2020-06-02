749c98551e831c9d0666b3b0dc7253206ac8baac
Taylor Burmeister <taylor.burmeister@datadoghq.com>
2020-06-02 14:10:34 -0400

    don't use with so PR is cleaner

--- bot.py
+++ bot.py
@@ -33,40 +33,41 @@ with open('./accounts.json') as f:
 
 while len(feed) != 0:
     for client in clients:
-        with cache.begin(write=True) as txn:
-            post = feed['items'].pop(0)
-            key = fmt.pack(post["id"])
-            if txn.get(key) is not None:
-                print("Duplicate image %d" % post["id"])
-                continue
+        txn = cache.begin(write=True)
+        post = feed['items'].pop(0)
+        key = fmt.pack(post["id"])
+        if txn.get(key) is not None:
+            print("Duplicate image %d" % post["id"])
+            continue
 
-            if 'image_versions2' in post:
-                try:
-                    url = post['image_versions2']['candidates'][0]['url']
-                    res = requests.post(
-                        os.getenv("CLOUD_FUNCTION_URL"), data={'img_url': url})
-                    json_res = res.json()
-                    if(json_res['solid']):
-                        code = post['code']
-                        if 'comment_count' in post and post['comment_count'] > 0:
-                            for comment in post['preview_comments']:
-                                if "please dont use the blacklivesmatter tag" in comment['text'].lower():
-                                    contains_comment = True
-                                    break
-                            if not contains_comment:
-                                print('Solid image found. Informing user on post %s' % code)
-                                client.post_comment(post['id'], MESSAGE)
-                            else:
-                                print('Bot has already commented on post: %s' % code)
-                            contains_comment = False
-                        else:
+        if 'image_versions2' in post:
+            try:
+                url = post['image_versions2']['candidates'][0]['url']
+                res = requests.post(
+                    os.getenv("CLOUD_FUNCTION_URL"), data={'img_url': url})
+                json_res = res.json()
+                if(json_res['solid']):
+                    code = post['code']
+                    if 'comment_count' in post and post['comment_count'] > 0:
+                        for comment in post['preview_comments']:
+                            if "please dont use the blacklivesmatter tag" in comment['text'].lower():
+                                contains_comment = True
+                                break
+                        if not contains_comment:
                             print('Solid image found. Informing user on post %s' % code)
                             client.post_comment(post['id'], MESSAGE)
-                except:
-                    print('Ran into an exception (IG may be rate limiting)')
-                    continue
+                        else:
+                            print('Bot has already commented on post: %s' % code)
+                        contains_comment = False
+                    else:
+                        print('Solid image found. Informing user on post %s' % code)
+                        client.post_comment(post['id'], MESSAGE)
+            except:
+                print('Ran into an exception (IG may be rate limiting)')
+                continue
 
-                txn.put(key, b"")
+            txn.put(key, b"")
+            txn.commit()
 
         if len(feed) == 1:
             feed = client.feed_tag('blacklivesmatter', client.generate_uuid())
