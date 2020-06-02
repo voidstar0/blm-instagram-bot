ca3b02643298d0ccb9981d7633e3417e6a10a973
simarpreet-singh <simsi@ucdavis.edu>
2020-06-02 12:12:25 -0700

    added logging for posts that have been commented on

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import requests
 import os
 import time
 import ssl
+import schedule
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
@@ -17,6 +18,7 @@ contains_comment = False
 
 MESSAGE = u'Hi, please don\'t use the blacklivesmatter tag as it is currently blocking important info from being shared. Please delete and repost with #BlackoutTuesday instead (Editing the caption wont work). If you want other ways to help please check out our bio. Thank you :)'
 
+
 with open('./accounts.json') as f:
     data = json.load(f)
     for acc in data:
@@ -28,32 +30,49 @@ with open('./accounts.json') as f:
 
 while len(feed) != 0:
     for client in clients:
-        post = feed['items'].pop(0)
-        if 'image_versions2' in post:
-            try:
-                url = post['image_versions2']['candidates'][0]['url']
-                res = requests.post(
-                    os.getenv("CLOUD_FUNCTION_URL"), data={'img_url': url})
-                json_res = res.json()
-                if(json_res['solid']):
-                    code = post['code']
-                    if 'comment_count' in post and post['comment_count'] > 0:
-                        for comment in post['preview_comments']:
-                            if "please dont use the blacklivesmatter tag" in comment['text'].lower():
-                                contains_comment = True
-                                break
-                        if not contains_comment:
-                            print('Solid image found. Informing user on post %s' % code)
-                            client.post_comment(post['id'], MESSAGE)
-                        else:
-                            print('Bot has already commented on post: %s' % code)
-                        contains_comment = False
-                    else:
-                        print('Solid image found. Informing user on post %s' % code)
-                        client.post_comment(post['id'], MESSAGE)
-            except:
-                print('Ran into an exception (IG may be rate limiting)')
-                continue
-        if len(feed) == 1:
-            feed = client.feed_tag('blacklivesmatter', client.generate_uuid())
+        for post in feed["items"]:
+            with open("./commentlog.txt", "r") as log:
+                cached = log.read()
+            print(cached)
+
+            if str(post['code']) not in cached:
+                print(f"{post['code']} not in cache")
+                if 'image_versions2' in post:
+                    try:
+                        url = post['image_versions2']['candidates'][0]['url']
+                        res = requests.post(
+                            os.getenv("CLOUD_FUNCTION_URL"), data={'img_url': url})
+                        json_res = res.json()
+                        if json_res['solid']:
+                            code = post['code']
+                            print(code)
+                            if 'comment_count' in post and post['comment_count'] > 0:
+                                for comment in post['preview_comments']:
+                                    if "please dont use the blacklivesmatter tag" in comment['text'].lower():
+                                        contains_comment = True
+                                        break
+                                if not contains_comment:
+                                    print('Solid image found. Informing user on post %s' % code)
+                                    client.post_comment(post['id'], MESSAGE)
+                                    # ADDED: log the url if we make a comment
+                                    print("Successful Comment")
+                                    with open("./commentlog.txt", "a") as log:
+                                        log.write(str(str(code)+","))
+                                    print("Post has been logged")
+                                else:
+                                    print('Bot has already commented on post: %s' % code)
+                                contains_comment = False
+                            else:
+                                print('Solid image found. Informing user on post %s' % code)
+                                client.post_comment(post['id'], MESSAGE)
+                                print("Successful Comment")
+                                # ADDED: log the url if we make a comment
+                                with open("./commentlog.txt", "a") as log:
+                                    log.write(str(str(code) + ","))
+                                print("Post has been logged")
+                    except Exception as e:
+                        print(f'Ran into an exception (IG may be rate limiting) {e}')
+                        continue
+            if len(feed) == 1:
+                feed = client.feed_tag('blacklivesmatter', client.generate_uuid())
 
